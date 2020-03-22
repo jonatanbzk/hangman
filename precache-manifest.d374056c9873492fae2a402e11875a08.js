f74d30b22b0201eec8a5cddc166de015a4541e52
Buzek Jonatan <buzek.jonatan@gmail.com>
2020-03-22 15:43:29 +0100

    Updates

---
+++ precache-manifest.d374056c9873492fae2a402e11875a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a825490ed58a006b68a6f612253855",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "e22ff173714255cf576a",
+    "url": "/hangman/static/css/main.44403006.chunk.css"
+  },
+  {
+    "revision": "94985cd906b30fa12d51",
+    "url": "/hangman/static/js/2.f7ec1d62.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hangman/static/js/2.f7ec1d62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e22ff173714255cf576a",
+    "url": "/hangman/static/js/main.e9a00f8c.chunk.js"
+  },
+  {
+    "revision": "f67066e55bc257b315e8",
+    "url": "/hangman/static/js/runtime-main.624cfea7.js"
+  }
+]);
